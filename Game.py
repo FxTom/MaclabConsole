2ae8efbe28a350d8f681d7a26435e8c689928b98
Fx <fxthomas000@gmail.com>
2017-12-02 21:05:43 +0100

    ajout d'un module

---
+++ Game.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*-coding:UTF-8 -*
+import Begin
+
+if __name__ == "__main__":
+    perso = "X"
+    pos_perso = [1,1]
+    level_1 =Begin.charge_lab("level")
+    Begin.game(level_1,perso,pos_perso)
