019332198855fb505028f590a5fc190acb8a0425
FXUbuntu <fxthomas000@gmail.com>
2017-12-08 10:16:36 +0100

    modif objet par gift

--- Begin.py
+++ Begin.py
@@ -1,6 +1,35 @@
 # -*-coding:utf-8 -*
 import os
 import sys
+import curses
+
+
+def init_curses(lignes,cols,pos):
+    curses.initscr()                 #initialisation du mode graphique
+    curses.noecho()                  #Desactivation de l'affichage
+    curses.cbreak()                  #Interception des touches tapez au clavier
+    curses.curs_set(0)               #Desactivation de l'affichage
+
+    window = curses.newwin(lignes,cols,pos[0],pos[1])   #Creation d'une fenetre (49 caractere de haut,79 de large positionner en 0,0 du terminal)
+    window.border(0)                                    #activation du trace de la bordure de la fenetre
+    window.keypad(1)                                    #activation du nombre de touches
+    return window                                       # attente de l'appui d'une touche
+
+def close_curses():
+    curses.echo()
+    curses.nocbreak()
+    curses.curs_set(1)
+    curses.endwin()
+
+def init_colors():
+    curses.start_color()
+    curses.init_pair(1,curses.COLOR_RED,curses.COLOR_BLACK)
+    curses.init_pair(2,curses.COLOR_GREEN,curses.COLOR_BLACK)
+    curses.init_pair(3,curses.COLOR_BLACK,curses.COLOR_BLUE)
+    return ["RED","GREEN","BLUE"]
+
+def color(code,l_color):
+    return curses.color_pair(l_color.index(code)+1)
 
 
 def load_lab(name):
@@ -12,17 +41,20 @@ def load_lab(name):
         item[i] = item[i].strip()
     return item
 
-def show_lab(lab,perso,pos_perso):
+def show_lab(lab,perso,pos_perso,window,coul):
     """show the labyrinthe
     lab: is the variable with the lab
     perso: symbole show characters
-    pos_perso: list show the position of chracters(line,column)"""
+    pos_perso: list show the position of chracters(line,column)
+    win: window graphic mode
+    coul: list of color for the graphic mode"""
     count_line = 0
     for i in lab:
         if count_line == pos_perso[1]:
-            print(i[0:pos_perso[0]]+ perso + i[pos_perso[0]+1:])
+            window.addstr(count_line+1,10,i[0:pos_perso[0]]+ perso + i[pos_perso[0]+1:])
+            window.addstr(count_line +1,10 + pos_perso[0],perso,color("RED",coul))
         else:
-            print(i)
+            window.addstr(count_line +1 ,10, i)
         count_line += 1
 
 def del_screen():
@@ -64,7 +96,7 @@ def verify_deplacement(lab,pos_col,pos_line,gift):
         return [pos_col,pos_line]
 
 
-def choice_gamers(lab,pos_perso,gift):
+def choice_gamers(lab,pos_perso,gift,window):
     """
     asks the player to choose his move and checks if possible.
     lab:labyrinthe
@@ -72,35 +104,39 @@ def choice_gamers(lab,pos_perso,gift):
     gift: data of the gamer
     """
     dep = None
-    choix = input("pour jouer utiliser z/s/q/d ou utilisez 'E' pour quitter  ")
-    if choix == "z":
+    choix = window.getch()
+    if choix == curses.KEY_UP:
         dep = verify_deplacement(lab,pos_perso[0],pos_perso[1]-1,gift)
-    elif choix == "s" :
+    elif choix == curses.KEY_DOWN:
         dep = verify_deplacement(lab,pos_perso[0],pos_perso[1] +1,gift)
-    elif choix =="q" :
+    elif choix == curses.KEY_LEFT :
         dep = verify_deplacement(lab,pos_perso[0]-1,pos_perso[1],gift)
-    elif choix =="d" :
+    elif choix == curses.KEY_RIGHT :
         dep = verify_deplacement(lab,pos_perso[0]+1,pos_perso[1],gift)
-    elif choix =="E" :
+    elif choix == 27 :
+        close_curses()
         os._exit(1)
-    if dep == None:
-        print("impossible move")
-    else :
+    if dep != None:
         pos_perso[0] = dep [0]
         pos_perso[1] = dep [1]
 
-def game(lab,objet,perso,pos_perso):
+def game(lab,gift,perso,pos_perso,window,coul):
     """
     The main loop of the game displays the labyrinth in
     its different states after the player moves.
     """
     while True:
-        del_screen()
-        show_lab(lab,perso,pos_perso)
-        choice_gamers(lab,pos_perso,objet)
-        if pos_perso == [-1,-1] and objet["gain"] < 3:
-            print("Et la seringue!!!!")
-            break
-        elif pos_perso == [-1,-1] and objet["gain"] == 3:
-            print("Bravo!!!! Boss anéantie!")
-            break
+        show_lab(lab,perso,pos_perso,window,coul)
+        choice_gamers(lab,pos_perso,objet,window)
+        if pos_perso == [-1,-1] and gift["gain"] < 3:
+            window.addstr(22,1,"Et la seringue!!!!",color("RED",coul))
+            window.addstr(23,1,"appuyez sur une touche pour continuer",color("RED",coul))
+            window.getch()
+            window.addstr(1,20," "*50)
+            window.addstr(1,21," "*50)
+        elif pos_perso == [-1,-1] and gift["gain"] == 3:
+            window.addstr(22,1,"BRAVO FELCITATIONS",color("GREEN",coul))
+            window.addstr(23,1,"appuyez sur une touche pour continuer",color("RED",coul))
+            window.getch()
+            window.addstr(1,20," "*50)
+            window.addstr(1,21, " "*50)
--- Game.py
+++ Game.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # -*-coding:UTF-8 -*
+
 import Begin
+import random
 from random import choice
 
 if __name__ == "__main__":
@@ -10,6 +12,10 @@ if __name__ == "__main__":
     gift  = {"gain":0}
     level_rand = ["level1","level2","level3","level4","level5","level6","level7"]
     levels = random.choice(level_rand)
+    window = Begin.init_curses(25,41,(0,0))
+    coul = Begin.init_colors()
 #Core program
     level = Begin.load_lab(levels)
-    Begin.game(level,gift,perso,pos_perso)
+    Begin.game(level,gift,perso,pos_perso,window,coul)
+    window.getch()
+    Begin.close_curses()
